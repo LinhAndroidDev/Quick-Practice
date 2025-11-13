3e75c2bb500abebc354a624c4838bc2ec2122f82
zyyx-linhnh <linhnh@zyyx.jp>
2025-11-13 16:20:34 +0700

    remove warning

--- app/src/main/java/com/example/quickpractice/ui/theme/screen/exam/component/dialog/DialogListQuestion.kt
+++ app/src/main/java/com/example/quickpractice/ui/theme/screen/exam/component/dialog/DialogListQuestion.kt
@@ -1,13 +1,8 @@
 package com.example.quickpractice.ui.theme.screen.exam.component.dialog
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.indication
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -15,10 +10,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -28,7 +21,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
-import com.example.quickpractice.ui.theme.Green
 import com.example.quickpractice.ui.theme.GreyEF
 import com.example.quickpractice.ui.theme.component.FooterView
 import com.example.quickpractice.ui.theme.screen.exam.component.ItemQuestionPreview
